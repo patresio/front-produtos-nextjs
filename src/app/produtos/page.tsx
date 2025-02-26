46ac0c1b0495e78fd164e08695ba6e9d1d307a7f
Ricardo Pereira <patresio@gmail.com>
2025-02-26 16:01:46 -0300

    requisitando dados do backend

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -6,7 +6,8 @@ const Produtos = () => {
   return (
     <div>
       {produtos.map((produto) => (
-        <div key={produto.codigo}>
+        <div key={produto.codigo} className="border border-zinc-300 rounded-sm h-40 flex flex-col justify-center items-center w-full px-6 py-2 gap-5 my-4">
+          <span>{produto.codigo}</span>
           <h1>{produto.nome}</h1>
           <p>{produto.preco}</p>
         </div>
